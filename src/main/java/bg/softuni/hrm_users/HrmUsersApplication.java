778591686287f98a2cb2ab36b648e4e3e5cf5e5c
Ivan Pachev <mr.pachev@abv.bg>
2024-07-06 15:00:27 +0300

    Added hrm-users

---
+++ src/main/java/bg/softuni/hrm_users/HrmUsersApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.hrm_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmUsersApplication.class, args);
+	}
+
+}
