6cd085b4ce6945c0dd2eb1e497a0917d89bf1f85
Ivan Pachev <mr.pachev@abv.bg>
2024-07-27 16:21:42 +0300

    Sorted DepartmentService.

--- src/main/java/bg/softuni/hrm_users/service/DepartmentService.java
+++ src/main/java/bg/softuni/hrm_users/service/DepartmentService.java
@@ -5,18 +5,36 @@ import bg.softuni.hrm_users.model.dto.*;
 import java.util.List;
 
 public interface DepartmentService {
-    //department
+    //get all departments names
     List<String> getAllDepartmentNames();
+
+    //get all departments
     List<DepartmentDTO> getAllDepartments();
-    void addDepartment(AddDepartmentDTO addDepartmentDTO);
+
+    //checking is exist department by name
     boolean isExistDepartment(String name);
+
+    //add new department
+    void addDepartment(AddDepartmentDTO addDepartmentDTO);
+
+    //get department by id
     DepartmentDTO getDepartmentByID(long id);
+
+    //edit department
     void editDepartment(DepartmentDTO departmentDTO);
+
+    //delete department
     void removeDepartment(long id);
 
-    //department employees
+    //get all department employees
     List<EmployeeDTO> allDepartmentEmployees(long id);
+
+    //get all employees names
     List<DepartmentEmployeeDTO> allEmployeesNames();
+
+    //add current employee in current department
     void addEmployee(DepartmentEmployeeDTO departmentEmployeeDTO, long idDep);
+
+    //delete employee in current department
     void removeEmployee(long idEm, long idDep);
 }
