dc5a17da0712549d5d4c45a11ba5b5bceec3a458
Ivan Pachev <mr.pachev@abv.bg>
2024-07-27 16:48:42 +0300

    Sorted PositionService.

--- src/main/java/bg/softuni/hrm_users/service/PositionService.java
+++ src/main/java/bg/softuni/hrm_users/service/PositionService.java
@@ -5,13 +5,30 @@ import bg.softuni.hrm_users.model.dto.*;
 import java.util.List;
 
 public interface PositionService {
+    //get all positions names
     List<String> getAllPositionName();
+
+    //get all positions
     List<PositionDTO> getAllPositionsDTOS();
-    List<EmployeeDTO> allPositionEmployees(long id);
-    PositionDTO getPositionById(long id);
+
+    //add new position
     void addNewPosition(AddPositionDTO addPositionDTO);
+
+    //get position by id
+    PositionDTO getPositionById(long id);
+
+    //edit position
     void editPosition(PositionDTO positionDTO);
+
+    //delete position
     void removePosition(long id);
+
+    //get all employees from current position
+    List<EmployeeDTO> allPositionEmployees(long id);
+
+    //add current employee in current position
     void addEmployee(PositionEmployeesDTO positionEmployeesDTO, long idPos);
+
+    //delete current employee from current position
     void removeEmployee(long idEm, long idPos);
 }
